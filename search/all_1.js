fdc099beb813fc6422df2f4c4198e1f83edd8b1b
Ryan Brown <brownrc@vt.edu>
2017-07-19 12:42:05 -0400

    updated 7/19/17

--- search/all_1.js
+++ search/all_1.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['acquisition_5fdate',['acquisition_date',['../classHSImage.html#a6ad4c3cd7687b52cf02e4788a8720052',1,'HSImage']]],
+  ['adapt_5funique',['adapt_unique',['../group__cplus__module.html#ga67c79a7cc9d33e2f433e2b54ecd71b03',1,'hsimage.h']]],
   ['addspecdatatoheader',['addSpecDataToHeader',['../classHSImage.html#a6a3f291264cea92414cf37da00d79f90',1,'HSImage']]],
   ['ambient_5fintensities',['ambient_intensities',['../classHSImage.html#a725abc308fcb988bd124d788829e22fa',1,'HSImage']]]
 ];
--- search/functions_1.js
+++ search/functions_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['adapt_5funique',['adapt_unique',['../group__cplus__module.html#ga67c79a7cc9d33e2f433e2b54ecd71b03',1,'hsimage.h']]],
   ['addspecdatatoheader',['addSpecDataToHeader',['../classHSImage.html#a6a3f291264cea92414cf37da00d79f90',1,'HSImage']]]
 ];
